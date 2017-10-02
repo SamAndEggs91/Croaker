f27b2fa14c74a16e2ac405fa06174d256de0f6c8
Sam Peterson <slpeterson91@gmail.com>
2017-10-02 18:40:57 -0400

    styling changes

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ document.getElementById("add").onclick = function () {
     li.innerHTML = text;
     document.getElementById("croaks").appendChild(li);
 }
+
+var dict = {
+    key1: "Donald Trump was sent to the moon today after insisting that it was made of cheese",
+    key2: "Kanye denies claims of interupting Kim during sex saying 'I was gonna let her finish'"
+};
