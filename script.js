7044b43ef5c059b872d78a25e64548d434fa3ba7
Sam Peterson <slpeterson91@gmail.com>
2017-10-04 14:31:14 -0400

    Started wiring up the dictionary

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ document.getElementById("add").onclick = function () {
     li.innerHTML = text;
     document.getElementById("croaks").appendChild(li);
 }
+
+var dict = {
+    1: "First post",
+    2: "Second post",
+    3: "Shit post",
+    4: "Food post",
+    5: "Fake news post",
+    6: "Animal post"
+};
+
+//window.alert(dict[2]);
+
+var autoGenPosts = Math.round(Math.random() * 6);
+
+window.alert(dict[autoGenPosts]);
